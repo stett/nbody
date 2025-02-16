89393a72058f2b3475f3ab426792b803a81e97fd
stett <stettjawa@gmail.com>
2025-02-16 15:11:27 -0800

    vulkan is hard to deal with

--- source/gpu.cpp
+++ source/gpu.cpp
@@ -1,10 +1,56 @@
 #if NBODY_GPU
+#include <iostream>
+#include <string>
 #include "gpu.h"
+#include "shaderc/shaderc.hpp"
 
 using nbody::GPU;
 
+static const std::string glsl_integrate = R"(
+    #version 450
+
+    struct Body
+    {
+        float mass;
+        float radius;
+        vec3 pos;
+        vec3 vel;
+        vec3 acc;
+    };
+
+    layout (binding = 0) uniform UBO
+    {
+        float dt;
+        int num_bodies;
+    } ubo;
+
+    layout(std140, binding = 1) buffer Pos
+    {
+       Body bodies[ ];
+    };
+
+    void main()
+    {
+        // get sim info
+        float dt = ubo.dt;
+        int index = int(gl_GlobalInvocationID);
+        vec3 pos = bodies[index].pos;
+        vec3 vel = bodies[index].vel;
+        vec3 acc = bodies[index].acc;
+
+        // integrate using semi-implicit euler
+        vel += acc * dt;
+        pos += vel * dt;
+
+        // update body info
+        bodies[index].pos = pos;
+        bodies[index].vel = vel;
+    }
+)";
+
 GPU::GPU()
     : instance(make_instance())
+    , physical_device(make_physical_device())
     , device(make_device())
     , command_pool(make_command_pool())
     , command_buffer(make_command_buffer())
@@ -13,12 +59,13 @@ GPU::GPU()
     , descriptor_set_layout(make_descriptor_set_layout())
     , descriptor_set(make_descriptor_set())
     , pipeline_layout(make_pipeline_layout())
-    , pipeline_integrate(make_pipeline(""))
-    , pipeline_accelerate(make_pipeline(""))
-{
-    setup_storage_buffer();
-    setup_command_buffer();
-}
+    , shader_integrate(make_shader(glsl_integrate))
+    //, shader_accelerate(make_shader(glsl_integrate))
+    , pipeline_integrate(make_pipeline(shader_integrate))
+    //, pipeline_accelerate(make_pipeline(shader_accelerate))
+    , buffer_bodies(make_buffer_bodies(0))
+    , buffer_uniforms(make_buffer_uniforms())
+{ }
 
 vk::raii::Instance GPU::make_instance()
 {
@@ -38,11 +85,14 @@ vk::raii::Instance GPU::make_instance()
     return { context, instance_create_info };
 }
 
-vk::raii::Device GPU::make_device()
+vk::raii::PhysicalDevice GPU::make_physical_device()
 {
     // get the first physical device
-    vk::raii::PhysicalDevice physical_device = vk::raii::PhysicalDevices(instance).front();
+    return vk::raii::PhysicalDevices(instance).front();
+}
 
+vk::raii::Device GPU::make_device()
+{
     // find the index of the first queue family that supports compute
     auto queue_family_properties = physical_device.getQueueFamilyProperties();
     auto compute_queue_family_properties =
@@ -77,20 +127,20 @@ vk::raii::CommandBuffer GPU::make_command_buffer()
 
 vk::raii::DescriptorPool GPU::make_descriptor_pool()
 {
+    // NOTE: I don't know how to calculate how many of these I actually need...
     std::vector<vk::DescriptorPoolSize> pool_sizes = {
-        vk::DescriptorPoolSize(vk::DescriptorType::eUniformBuffer, 1),
-        vk::DescriptorPoolSize(vk::DescriptorType::eStorageBuffer, 1)
+        vk::DescriptorPoolSize(vk::DescriptorType::eUniformBuffer, 10),
+        vk::DescriptorPoolSize(vk::DescriptorType::eStorageBuffer, 10)
     };
-    vk::DescriptorPoolCreateInfo create_info({ vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet }, 2, pool_sizes);
-    return { device, create_info };
+    return { device, { { vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet }, 10, pool_sizes } };
 }
 
 vk::raii::DescriptorSetLayout GPU::make_descriptor_set_layout()
 {
     std::vector<vk::DescriptorSetLayoutBinding> bindings =
     {
-        {0, vk::DescriptorType::eStorageBuffer, 1, vk::ShaderStageFlagBits::eCompute, nullptr},
-        {1, vk::DescriptorType::eUniformBuffer, 1, vk::ShaderStageFlagBits::eCompute, nullptr},
+        { 0, vk::DescriptorType::eUniformBuffer, 1, vk::ShaderStageFlagBits::eCompute, nullptr },
+        { 1, vk::DescriptorType::eStorageBuffer, 1, vk::ShaderStageFlagBits::eCompute, nullptr },
     };
 
     return { device, { { }, bindings } };
@@ -98,236 +148,126 @@ vk::raii::DescriptorSetLayout GPU::make_descriptor_set_layout()
 
 vk::raii::DescriptorSet GPU::make_descriptor_set()
 {
-    vk::DescriptorSetAllocateInfo allocInfo(
-        *descriptor_pool,       // The descriptor pool handle
-        1,                      // Number of descriptor sets to allocate
-        &*descriptor_set_layout // Pointer to the descriptor set layout
-    );
-
-    // Allocate the descriptor set(s) using the RAII device method:
-    std::vector<vk::raii::DescriptorSet> descriptor_sets = device.allocateDescriptorSets(allocInfo);
-    assert(descriptor_sets.size() == 1);
-    return std::move(descriptor_sets.front());
+    vk::DescriptorSetAllocateInfo descriptorSetAllocateInfo(descriptor_pool, *descriptor_set_layout);
+    return std::move(vk::raii::DescriptorSets(device, descriptorSetAllocateInfo).front());
 }
 
 vk::raii::PipelineLayout GPU::make_pipeline_layout()
 {
-    vk::PipelineLayoutCreateInfo pipeline_layout_create_info({ }, { *descriptor_set_layout }, { });
-    return { device, pipeline_layout_create_info };
+    return { device, { { }, { *descriptor_set_layout } } };
 }
 
-vk::raii::Pipeline GPU::make_pipeline(const std::string& code)
+nbody::Buffer GPU::make_buffer_bodies(uint32_t new_num_bodies)
 {
-    // create the shader module
-    vk::ShaderModuleCreateInfo shader_module_create_info({ }, code.size(), reinterpret_cast<const uint32_t*>(code.data()));
-    vk::ShaderModule shader_module = device.createShaderModule(shader_module_create_info);
+    return { physical_device, device, sizeof(Body) * new_num_bodies, vk::BufferUsageFlagBits::eStorageBuffer };
+}
 
-    // We want to use as much shared memory for the compute shader invocations as available, so we calculate it based on the device limits and pass it to the shader via specialization constants
-    //uint32_t sharedDataSize = std::min((uint32_t)1024, (uint32_t)(vulkanDevice->properties.limits.maxComputeSharedMemorySize / sizeof(glm::vec4)));
-    //vk::SpecializationMapEntry specializationMapEntry = vks::initializers::specializationMapEntry(0, 0, sizeof(uint32_t));
-    //vk::SpecializationInfo specialization_info = vks::initializers::specializationInfo(1, &specializationMapEntry, sizeof(int32_t), &sharedDataSize);
-    //computePipelineCreateInfo.stage.pSpecializationInfo = &specializationInfo;
+nbody::Buffer GPU::make_buffer_uniforms()
+{
+    return { physical_device, device, sizeof(Uniforms), vk::BufferUsageFlagBits::eUniformBuffer };
+}
 
+void GPU::integrate(std::vector<Body>& bodies, const float dt)
+{
+    {
+        // update uniforms
+        uniforms.dt = dt;
+        uniforms.num_bodies = bodies.size();
+        //vk::DescriptorBufferInfo descriptor_buffer_info(buffer_uniforms, 0, sizeof(Uniforms));
+        //vk::WriteDescriptorSet write_descriptor_set(descriptor_set, 0, 0, vk::DescriptorType::eUniformBuffer, { }, descriptor_buffer_info);
+        //device.updateDescriptorSets(write_descriptor_set, nullptr);
+    }
+}
+
+vk::raii::ShaderModule GPU::make_shader(const std::string& glsl)
+{
+    const std::vector<uint32_t> spv = glsl_to_spv(glsl);
+    return { device, { vk::ShaderModuleCreateFlags(), spv } };
+}
+
+vk::raii::Pipeline GPU::make_pipeline(vk::raii::ShaderModule& shader)
+{
     // create the pipeline
-    vk::PipelineShaderStageCreateInfo shader_stage_create_info({ }, vk::ShaderStageFlagBits::eCompute, shader_module, "main");
+    vk::PipelineShaderStageCreateInfo shader_stage_create_info({ }, vk::ShaderStageFlagBits::eCompute, shader, "main");
     vk::ComputePipelineCreateInfo compute_pipeline_create_info({ }, shader_stage_create_info, *pipeline_layout, { }, -1);
     return { device, nullptr, compute_pipeline_create_info };
 }
 
-void GPU::setup_storage_buffer()
+nbody::Buffer::Buffer(
+    vk::raii::PhysicalDevice& physical_device,
+    vk::raii::Device& device,
+    vk::DeviceSize size,
+    vk::BufferUsageFlags usage,
+    vk::MemoryPropertyFlags properties)
+    : size(size)
+    , usage(usage)
+    , properties(properties)
+    , device(device)
+    , buffer(device, { { }, size, vk::BufferUsageFlagBits::eStorageBuffer })
+    , memory(nullptr)
 {
-    /*
-    // ...
-    // We mark a few particles as attractors that move along a given path, these will pull in the other particles
-    std::vector<glm::vec3> attractors = {
-            glm::vec3(5.0f, 0.0f, 0.0f),
-            glm::vec3(-5.0f, 0.0f, 0.0f),
-            glm::vec3(0.0f, 0.0f, 5.0f),
-            glm::vec3(0.0f, 0.0f, -5.0f),
-            glm::vec3(0.0f, 4.0f, 0.0f),
-            glm::vec3(0.0f, -8.0f, 0.0f),
-    };
-
-    numParticles = static_cast<uint32_t>(attractors.size()) * PARTICLES_PER_ATTRACTOR;
-
-    // Initial particle positions
-    std::vector<Particle> particleBuffer(numParticles);
-
-    std::default_random_engine rndEngine(benchmark.active ? 0 : (unsigned)time(nullptr));
-    std::normal_distribution<float> rndDist(0.0f, 1.0f);
-
-    for (uint32_t i = 0; i < static_cast<uint32_t>(attractors.size()); i++)
-    {
-        for (uint32_t j = 0; j < PARTICLES_PER_ATTRACTOR; j++)
-        {
-            Particle& particle = particleBuffer[i * PARTICLES_PER_ATTRACTOR + j];
-
-            // First particle in group as heavy center of gravity
-            if (j == 0)
-            {
-                particle.pos = glm::vec4(attractors[i] * 1.5f, 90000.0f);
-                particle.vel = glm::vec4(glm::vec4(0.0f));
-            }
-            else
-            {
-                // Position
-                glm::vec3 position(attractors[i] + glm::vec3(rndDist(rndEngine), rndDist(rndEngine), rndDist(rndEngine)) * 0.75f);
-                float len = glm::length(glm::normalize(position - attractors[i]));
-                position.y *= 2.0f - (len * len);
-
-                // Velocity
-                glm::vec3 angular = glm::vec3(0.5f, 1.5f, 0.5f) * (((i % 2) == 0) ? 1.0f : -1.0f);
-                glm::vec3 velocity = glm::cross((position - attractors[i]), angular) + glm::vec3(rndDist(rndEngine), rndDist(rndEngine), rndDist(rndEngine) * 0.025f);
-
-                float mass = (rndDist(rndEngine) * 0.5f + 0.5f) * 75.0f;
-                particle.pos = glm::vec4(position, mass);
-                particle.vel = glm::vec4(velocity, 0.0f);
-            }
-
-            // Color gradient offset
-            particle.vel.w = (float)i * 1.0f / static_cast<uint32_t>(attractors.size());
-        }
-    }
-
-    compute.uniformData.particleCount = numParticles;
-
-    VkDeviceSize storageBufferSize = particleBuffer.size() * sizeof(Particle);
-
-    // Staging
-    // SSBO won't be changed on the host after upload so copy to device local memory
-
-    vks::Buffer stagingBuffer;
-
-    vulkanDevice->createBuffer(VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, &stagingBuffer, storageBufferSize, particleBuffer.data());
-    // The SSBO will be used as a storage buffer for the compute pipeline and as a vertex buffer in the graphics pipeline
-    vulkanDevice->createBuffer(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, &storageBuffer, storageBufferSize);
-
-    // Copy from staging buffer to storage buffer
-    VkCommandBuffer copyCmd = vulkanDevice->createCommandBuffer(VK_COMMAND_BUFFER_LEVEL_PRIMARY, true);
-    VkBufferCopy copyRegion = {};
-    copyRegion.size = storageBufferSize;
-    vkCmdCopyBuffer(copyCmd, stagingBuffer.buffer, storageBuffer.buffer, 1, &copyRegion);
-    // Execute a transfer barrier to the compute queue, if necessary
-    if (graphics.queueFamilyIndex != compute.queueFamilyIndex)
-    {
-        VkBufferMemoryBarrier buffer_barrier =
-                {
-                        VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER,
-                        nullptr,
-                        VK_ACCESS_VERTEX_ATTRIBUTE_READ_BIT,
-                        0,
-                        graphics.queueFamilyIndex,
-                        compute.queueFamilyIndex,
-                        storageBuffer.buffer,
-                        0,
-                        storageBuffer.size
-                };
-
-        vkCmdPipelineBarrier(
-                copyCmd,
-                VK_PIPELINE_STAGE_VERTEX_INPUT_BIT,
-                VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
-                0,
-                0, nullptr,
-                1, &buffer_barrier,
-                0, nullptr);
-    }
-    vulkanDevice->flushCommandBuffer(copyCmd, queue, true);
-
-    stagingBuffer.destroy();
-    */
+    if (size < 1) { return; }
+    GPU::alloc_device_memory(device, physical_device.getMemoryProperties(), buffer.getMemoryRequirements(), properties);
+    buffer.bindMemory(memory, 0);
 }
 
-void GPU::setup_command_buffer()
+std::vector<uint32_t> GPU::glsl_to_spv(const std::string& glsl, const std::string& identifier)
 {
-    command_buffer.begin(vk::CommandBufferBeginInfo());
+    // create a shaderc compiler instance & options object
+    shaderc::Compiler compiler;
+    shaderc::CompileOptions options; // todo: set optimization level?
+    options.SetTargetEnvironment(shaderc_target_env_vulkan, shaderc_env_version_vulkan_1_4);
+    options.SetGenerateDebugInfo();
+    options.SetOptimizationLevel(shaderc_optimization_level_zero);
 
-    /*
+    // compile the glsl
+    shaderc::SpvCompilationResult result = compiler.CompileGlslToSpv(
+        glsl,
+        shaderc_shader_kind::shaderc_glsl_compute_shader,
+        identifier.c_str(),
+        options
+    );
 
-    // Acquire barrier
-    if (graphics.queueFamilyIndex != compute.queueFamilyIndex)
+    // Check for compilation errors.
+    if (result.GetCompilationStatus() != shaderc_compilation_status_success)
     {
-        VkBufferMemoryBarrier buffer_barrier =
-                {
-                        VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER,
-                        nullptr,
-                        0,
-                        VK_ACCESS_SHADER_WRITE_BIT,
-                        graphics.queueFamilyIndex,
-                        compute.queueFamilyIndex,
-                        storageBuffer.buffer,
-                        0,
-                        storageBuffer.size
-                };
-
-        vkCmdPipelineBarrier(
-                compute.commandBuffer,
-                VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
-                VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT,
-                0,
-                0, nullptr,
-                1, &buffer_barrier,
-                0, nullptr);
+        std::cerr << "Shader compilation error: " << result.GetErrorMessage() << std::endl;
+        assert(false);
+        return { };
     }
 
-    // First pass: Calculate particle movement
-    // -------------------------------------------------------------------------------------------------------
-    vkCmdBindPipeline(compute.commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, compute.pipelineCalculate);
-    vkCmdBindDescriptorSets(compute.commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, compute.pipelineLayout, 0, 1, &compute.descriptorSet, 0, 0);
-    vkCmdDispatch(compute.commandBuffer, numParticles / 256, 1, 1);
+    // Get the SPIR-V code as a vector of uint32_t.
+    return { result.cbegin(), result.cend() };
+}
 
-    // Add memory barrier to ensure that the computer shader has finished writing to the buffer
-    VkBufferMemoryBarrier bufferBarrier = vks::initializers::bufferMemoryBarrier();
-    bufferBarrier.buffer = storageBuffer.buffer;
-    bufferBarrier.size = storageBuffer.descriptor.range;
-    bufferBarrier.srcAccessMask = VK_ACCESS_SHADER_WRITE_BIT;
-    bufferBarrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
-    // Transfer ownership if compute and graphics queue family indices differ
-    bufferBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-    bufferBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-
-    vkCmdPipelineBarrier(
-            compute.commandBuffer,
-            VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT,
-            VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT,
-            VK_FLAGS_NONE,
-            0, nullptr,
-            1, &bufferBarrier,
-            0, nullptr);
-
-    // Second pass: Integrate particles
-    // -------------------------------------------------------------------------------------------------------
-    vkCmdBindPipeline(compute.commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, compute.pipelineIntegrate);
-    vkCmdDispatch(compute.commandBuffer, numParticles / 256, 1, 1);
-
-    // Release barrier
-    if (graphics.queueFamilyIndex != compute.queueFamilyIndex)
+uint32_t GPU::find_memory_type(
+    vk::PhysicalDeviceMemoryProperties const& memoryProperties,
+    uint32_t typeBits,
+    vk::MemoryPropertyFlags requirementsMask)
+{
+    uint32_t typeIndex = uint32_t( ~0 );
+    for ( uint32_t i = 0; i < memoryProperties.memoryTypeCount; i++ )
     {
-        VkBufferMemoryBarrier buffer_barrier =
-                {
-                        VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER,
-                        nullptr,
-                        VK_ACCESS_SHADER_WRITE_BIT,
-                        0,
-                        compute.queueFamilyIndex,
-                        graphics.queueFamilyIndex,
-                        storageBuffer.buffer,
-                        0,
-                        storageBuffer.size
-                };
-
-        vkCmdPipelineBarrier(
-                compute.commandBuffer,
-                VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT,
-                VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
-                0,
-                0, nullptr,
-                1, &buffer_barrier,
-                0, nullptr);
+        if ( ( typeBits & 1 ) && ( ( memoryProperties.memoryTypes[i].propertyFlags & requirementsMask ) == requirementsMask ) )
+        {
+            typeIndex = i;
+            break;
+        }
+        typeBits >>= 1;
     }
-    */
+    assert( typeIndex != uint32_t( ~0 ) );
+    return typeIndex;
+}
 
-    command_buffer.end();
+vk::raii::DeviceMemory GPU::alloc_device_memory(
+    vk::raii::Device const& device,
+    vk::PhysicalDeviceMemoryProperties const& memoryProperties,
+    vk::MemoryRequirements const& memoryRequirements,
+    vk::MemoryPropertyFlags memoryPropertyFlags)
+{
+    uint32_t memoryTypeIndex = find_memory_type( memoryProperties, memoryRequirements.memoryTypeBits, memoryPropertyFlags );
+    vk::MemoryAllocateInfo memoryAllocateInfo( memoryRequirements.size, memoryTypeIndex );
+    return { device, memoryAllocateInfo };
 }
 
 #endif
--- source/gpu.h
+++ source/gpu.h
@@ -1,21 +1,48 @@
 #pragma once
 #if NBODY_GPU
-#include <iostream>
+#include <vector>
 #include "vulkan/vulkan_raii.hpp"
+#include "nbody/body.h"
 
 namespace nbody
 {
+    struct Uniforms
+    {
+        float dt;
+        int num_bodies;
+    };
+
+    struct Buffer
+    {
+        vk::DeviceSize size;
+        vk::BufferUsageFlags usage;
+        vk::MemoryPropertyFlags properties;
+        vk::raii::Device& device;
+        vk::raii::Buffer buffer;
+        vk::raii::DeviceMemory memory;
+
+        Buffer(
+            vk::raii::PhysicalDevice& physical_device,
+            vk::raii::Device& device,
+            vk::DeviceSize size,
+            vk::BufferUsageFlags usage,
+            vk::MemoryPropertyFlags properties = vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent);
+    };
+
     class GPU
     {
     public:
 
         GPU();
 
+        void integrate(std::vector<Body>& bodies, const float dt);
+
     private:
 
         // RAII vk objects
         vk::raii::Context context;
         vk::raii::Instance instance;
+        vk::raii::PhysicalDevice physical_device;
         vk::raii::Device device;
         vk::raii::CommandPool command_pool;
         vk::raii::CommandBuffer command_buffer;
@@ -24,14 +51,22 @@ namespace nbody
         vk::raii::DescriptorSetLayout descriptor_set_layout;
         vk::raii::DescriptorSet descriptor_set;
         vk::raii::PipelineLayout pipeline_layout;
+        vk::raii::ShaderModule shader_integrate;
+        //vk::raii::ShaderModule shader_accelerate;
         vk::raii::Pipeline pipeline_integrate;
-        vk::raii::Pipeline pipeline_accelerate;
+        //vk::raii::Pipeline pipeline_accelerate;
+        nbody::Buffer buffer_bodies;
+        nbody::Buffer buffer_uniforms;
 
         // cached vk data
         uint32_t queue_family_index;
 
+        // runtime data
+        Uniforms uniforms = { 0.f, 0 };
+
         // member initializer functions
         vk::raii::Instance make_instance();
+        vk::raii::PhysicalDevice make_physical_device();
         vk::raii::Device make_device();
         vk::raii::CommandPool make_command_pool();
         vk::raii::CommandBuffer make_command_buffer();
@@ -39,11 +74,24 @@ namespace nbody
         vk::raii::DescriptorSetLayout make_descriptor_set_layout();
         vk::raii::DescriptorSet make_descriptor_set();
         vk::raii::PipelineLayout make_pipeline_layout();
-        vk::raii::Pipeline make_pipeline(const std::string& code);
+        nbody::Buffer make_buffer_bodies(uint32_t new_num_bodies);
+        nbody::Buffer make_buffer_uniforms();
 
-        // setup functions
-        void setup_storage_buffer();
-        void setup_command_buffer();
+        // helper functions
+        vk::raii::ShaderModule make_shader(const std::string& glsl);
+        vk::raii::Pipeline make_pipeline(vk::raii::ShaderModule& shader);
+
+    public:
+
+        static std::vector<uint32_t> glsl_to_spv(const std::string& glsl, const std::string& identifier = "unidentified");
+
+        static uint32_t find_memory_type(vk::PhysicalDeviceMemoryProperties const& memoryProperties, uint32_t typeBits, vk::MemoryPropertyFlags requirementsMask);
+
+        static vk::raii::DeviceMemory alloc_device_memory(
+                vk::raii::Device const& device,
+                vk::PhysicalDeviceMemoryProperties const& memoryProperties,
+                vk::MemoryRequirements const& memoryRequirements,
+                vk::MemoryPropertyFlags memoryPropertyFlags);
     };
 }
 #endif
