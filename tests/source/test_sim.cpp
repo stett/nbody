accd77c5fb961b237de88da20165d3f236c61855
stett <stettjawa@gmail.com>
2025-02-23 11:31:11 -0800

    making glsl -> spv into a preprocessor step

--- tests/source/test_sim.cpp
+++ tests/source/test_sim.cpp
@@ -18,49 +18,10 @@ TEST_CASE("orbit doesn't decay", "[sim]")
     // advance the simulation by 100 ticks at 60hz
     for (size_t i = 0; i < 10; ++i)
     {
-        /*
-        std::stringstream ss;
-        for (size_t j = 0; j < 10; ++j)
-        {
-            const nbody::Body& b = sim.bodies[j];
-            ss << "(" << b.pos.x << "," << b.pos.y << "," << b.pos.z << ") ";
-        }
-        */
-
-        /*
-        const float x1 = sim.bodies[1].pos.x;
-        const float y1 = sim.bodies[1].pos.y;
-        const float z1 = sim.bodies[1].pos.z;
-        const float x2 = sim.bodies[2].pos.x;
-        const float y2 = sim.bodies[2].pos.y;
-        const float z2 = sim.bodies[2].pos.z;
-        const float x3 = sim.bodies[3].pos.x;
-        const float y3 = sim.bodies[3].pos.y;
-        const float z3 = sim.bodies[3].pos.z;
-        const float x4 = sim.bodies[4].pos.x;
-        const float y4 = sim.bodies[4].pos.y;
-        const float z4 = sim.bodies[4].pos.z;
-        */
-
         sim.update(1.f / 120.f);
-
-        const float ax1 = sim.bodies[1].acc.x;
-        const float ay1 = sim.bodies[1].acc.y;
-        const float az1 = sim.bodies[1].acc.z;
-        const float ax2 = sim.bodies[2].acc.x;
-        const float ay2 = sim.bodies[2].acc.y;
-        const float az2 = sim.bodies[2].acc.z;
-        const float ax3 = sim.bodies[3].acc.x;
-        const float ay3 = sim.bodies[3].acc.y;
-        const float az3 = sim.bodies[3].acc.z;
-        const float ax4 = sim.bodies[4].acc.x;
-        const float ay4 = sim.bodies[4].acc.y;
-        const float az4 = sim.bodies[4].acc.z;
     }
 
-    /*
     // make sure all of the stars are still within the original radius
     for (const nbody::Body& body : sim.bodies)
         REQUIRE(body.pos.dist_sq({0,0,0}) <= rad * rad);
-        */
 }
