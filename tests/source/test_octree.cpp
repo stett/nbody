ccc7c25f58928aebfaa5a204a2c24b3d20a7679e
stett <stettjawa@gmail.com>
2026-08-30 23:10:45 -0400

    new failing test - num_octree_nodes computed incorrectly

--- tests/source/test_octree.cpp
+++ tests/source/test_octree.cpp
@@ -7,15 +7,18 @@
 #include "detail/octree.h"
 #include "detail/morton.h"
 
-TEST_CASE("create quadtree (flat octree)", "[octree]")
+namespace
 {
-    using std::vector;
-    using std::ranges::sort;
-    using std::ranges::transform;
-    using std::exclusive_scan;
-    using namespace nbody;
-    using namespace nbody::detail;
+	using std::vector;
+	using std::ranges::sort;
+	using std::ranges::transform;
+	using std::exclusive_scan;
+	using namespace nbody;
+	using namespace nbody::detail;
+}
 
+TEST_CASE("create 3-element quadtree (flat octree)", "[octree]")
+{
     // morton codes, sorted, manually generated from a set of 2d coordinates:
     // 0. (1, 5) -> (001, 101) -> 010011
     // 1. (4, 3) -> (100, 011) -> 100101
@@ -33,13 +36,6 @@ TEST_CASE("create quadtree (flat octree)", "[octree]")
     //
     // two bits per level for a quadtree, and three levels of them: the codes below are written
     // as six significant bits, which Morton left-aligns into the word
-    /*using MortonT = detail::Morton<uint32_t, 2, 6>;
-    const vector<MortonT> keys{
-        0b010011,
-        0b100101,
-        0b100111,
-    };
-    */
     using MortonT = detail::Morton<uint32_t, 2, 4>;
     const vector<MortonT> keys{
         0b0110,
@@ -48,7 +44,6 @@ TEST_CASE("create quadtree (flat octree)", "[octree]")
     };
 
     // build the octree
-    //scalar::build_octree<MortonT>(keys, radix_nodes, radix_parents, node_counts, node_count_totals, offsets, octree_nodes);
 	vector<OctreeNode> octree_nodes = scalar::build_octree<MortonT>(keys);
 	REQUIRE(octree_nodes.size() == 5);
 
@@ -75,4 +70,31 @@ TEST_CASE("create quadtree (flat octree)", "[octree]")
 	REQUIRE(octree_nodes[2] == OctreeNode{ .parent = 0, .next = 0, .child = 3, .is_leaf = 0 });
 	REQUIRE(octree_nodes[3] == OctreeNode{ .parent = 2, .next = 4, .child = 1, .is_leaf = 1 });
 	REQUIRE(octree_nodes[4] == OctreeNode{ .parent = 2, .next = 0, .child = 2, .is_leaf = 1 });
+}
+
+TEST_CASE("create 6-element quadtree (flat octree)", "[octree]")
+{
+	using MortonT = detail::Morton<uint32_t, 2, 6>;
+	const vector<MortonT> keys{
+		000111,
+		010100,
+		100011,
+		101000,
+		110001,
+		110100
+	};
+
+	// build the octree
+	vector<OctreeNode> octree_nodes = scalar::build_octree<MortonT>(keys);
+	REQUIRE(octree_nodes.size() == 9);
+
+	REQUIRE(octree_nodes[0] == OctreeNode{ .parent = 0, .next = 0, .child = 1, .is_leaf = 0 });
+	REQUIRE(octree_nodes[1] == OctreeNode{ .parent = 0, .next = 2, .child = 0, .is_leaf = 1 });
+	REQUIRE(octree_nodes[2] == OctreeNode{ .parent = 0, .next = 3, .child = 1, .is_leaf = 1 });
+	REQUIRE(octree_nodes[3] == OctreeNode{ .parent = 0, .next = 6, .child = 4, .is_leaf = 0 });
+	REQUIRE(octree_nodes[4] == OctreeNode{ .parent = 3, .next = 5, .child = 2, .is_leaf = 1 });
+	REQUIRE(octree_nodes[5] == OctreeNode{ .parent = 3, .next = 6, .child = 3, .is_leaf = 1 });
+	REQUIRE(octree_nodes[6] == OctreeNode{ .parent = 0, .next = 0, .child = 7, .is_leaf = 0 });
+	REQUIRE(octree_nodes[7] == OctreeNode{ .parent = 6, .next = 8, .child = 4, .is_leaf = 1 });
+	REQUIRE(octree_nodes[8] == OctreeNode{ .parent = 6, .next = 0, .child = 5, .is_leaf = 1 });
 }
